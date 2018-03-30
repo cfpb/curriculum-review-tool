171c9f780cf9cfd43350da25b003f8defaa87a8c
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-03-30 16:27:11 -0400

    Update front-end setup script

--- setup.sh
+++ setup.sh
@@ -1,49 +1,82 @@
-#!/bin/sh
+#!/bin/bash
+
+# ==========================================================================
+# Setup script for installing project dependencies.
+# NOTE: Run this script while in the project root directory.
+#       It will not run correctly when run from another directory.
+# ==========================================================================
 
 # Set script to exit on any errors.
 set -e
 
-# Initialize project dependency directories and the shrinkwrap file location.
-init(){
-  NODE_DIR=node_modules
-  SHRINKWRAP=npm-shrinkwrap.json
+# Initialize project dependency directories.
+init() {
+  if [ -f "package-lock.json" ]; then
+    DEP_CHECKSUM=$(cat package*.json | shasum -a 256)
+  else
+    DEP_CHECKSUM=$(cat package.json | shasum -a 256)
+  fi
 
-  echo 'npm components directory:' $NODE_DIR
+  if [[ "$(node -v)" != 'v8.'* ]]; then
+    printf "\033[1;31mPlease install Node 8.x: 'nvm install 8'\033[0m\n"
+  fi
+
+  NODE_DIR=node_modules
+  echo "npm components directory: $NODE_DIR"
 }
 
 # Clean project dependencies.
-clean(){
-  # If the node directory already exist,
-  # clear them so we know we're working with a clean
+clean() {
+  # If the node directory already exists,
+  # clear it so we know we're working with a clean
   # slate of the dependencies listed in package.json.
   if [ -d $NODE_DIR ]; then
-    echo 'Removing project dependency directories...'
+    echo 'Removing project dependency directories…'
     rm -rf $NODE_DIR
+    echo 'Project dependencies have been removed.'
   fi
-  echo 'Project dependencies have been removed.'
 }
 
 # Install project dependencies.
-install(){
-  echo 'Installing project dependencies...'
-  if [ -f $SHRINKWRAP ]; then
-    echo 'Removing previous shrinkwrapped dependency settings...'
-    rm -f $SHRINKWRAP
+install() {
+  echo 'Installing front-end dependencies…'
+  npm install -d --loglevel warn
+}
+
+# Add a checksum file
+checksum() {
+  echo -n "$DEP_CHECKSUM" > $NODE_DIR/CHECKSUM
+}
+
+# If the node directory exists, $NODE_DIR/CHECKSUM exists, and
+# the contents DO NOT match the checksum of package.json, clear
+# $NODE_DIR so we know we're working with a clean slate of the
+# dependencies listed in package.json.
+clean_and_install() {
+  if [ ! -f $NODE_DIR/CHECKSUM ] ||
+     [ "$DEP_CHECKSUM" != "$(cat $NODE_DIR/CHECKSUM)" ]; then
+    clean
+    install
+    checksum
+  else
+    echo 'Dependencies are up to date.'
   fi
-  npm install
-  echo 'Shrinkwrapping project dependencies...'
-  npm prune
-  npm shrinkwrap --dev
 }
 
 # Run tasks to build the project for distribution.
-build(){
-  echo 'Building project...'
-  gulp clean
+build() {
+  echo 'Building project…'
   gulp build
 }
 
-init
-clean
-install
-build
+# Execute requested (or all) functions.
+if [ "$1" == "init" ]; then
+  init ""
+  clean_and_install
+elif [ "$1" == "build" ]; then
+  build
+else
+  init "$1"
+  clean_and_install
+  build
+fi
