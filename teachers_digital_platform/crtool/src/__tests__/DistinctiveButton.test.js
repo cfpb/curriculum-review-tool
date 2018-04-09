c534642199443fb13b5d1ea54eb741f1fed218c6
Allen Tuggle <allen.tuggle@cfpb.gov>
2018-04-09 14:12:53 -0400

    UNIT-TEST: Added beforeAll() and afterAll() methods to test

--- teachers_digital_platform/crtool/src/__tests__/DistinctiveButton.test.js
+++ teachers_digital_platform/crtool/src/__tests__/DistinctiveButton.test.js
@@ -3,7 +3,9 @@ import C from '../js/constants';
 import DistinctiveButton from '../js/components/distinctives/DistinctiveButton';
 import renderer from 'react-test-renderer';
 
-const distinctiveProps = {
+let result;
+let distinctiveButtonClickedValue = "false";
+let distinctiveProps = {
   title:"Quality",
   criteria:"5 criteria",
   estimatedtime:"Est. time 30 min",
@@ -13,33 +15,36 @@ const distinctiveProps = {
   distinctiveClicked:distinctiveClicked.bind(this),
 }
 
-let distinctiveButtonClickedValue = "false";
 function distinctiveClicked(name) {
   distinctiveButtonClickedValue = name;
 }
 
-test('DistinctiveButton uses state to populate values', () => {
-  const component = renderer.create(
-    <DistinctiveButton {...distinctiveProps}/>,
+beforeAll(() => {
+  result = renderer.create(
+    <DistinctiveButton 
+      distinctiveClicked={distinctiveClicked.bind(this)} 
+      {...distinctiveProps}
+    />,
   );
+});
 
-    let tree = component.toJSON();
+afterAll(() => {
+  result.unmount();
+});
+
+test('DistinctiveButton uses state to populate values', () => {
+    let tree = result.toJSON();
     expect(tree).toMatchSnapshot();
 });
 
 test('DistinctiveButton correctly invokes onClick', () => {
-  const component = renderer.create(
-    <DistinctiveButton {...distinctiveProps}/>,
-  );
-
-  // manually trigger the callback
-  let tree = component.toJSON();
+  let tree = result.toJSON();
   tree.props.onClick(C.QUALITY_DISTINCTIVE);
 
   // makes sure that the function distinctiveClicked is called with the proper parameters
   expect(distinctiveButtonClickedValue).toBe(C.QUALITY_DISTINCTIVE);
 
   // re-rendering
-  tree = component.toJSON();
+  tree = result.toJSON();
   expect(tree).toMatchSnapshot();
 });
