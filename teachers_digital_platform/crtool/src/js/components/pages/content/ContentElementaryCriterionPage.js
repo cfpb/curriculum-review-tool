5a76fd9c707b8fd14544f8ce1dd6ba2221f51d10
Russell Stoll <russell.stoll@cfpb.gov>
2018-04-12 16:00:41 -0400

    Removing "completed" svg code

--- teachers_digital_platform/crtool/src/js/components/pages/content/ContentElementaryCriterionPage.js
+++ teachers_digital_platform/crtool/src/js/components/pages/content/ContentElementaryCriterionPage.js
@@ -47,10 +47,7 @@ export default class ContentElementaryCriterionPage extends React.Component {
                         u-mb30
                         u-mt30" />
             <div className="block block__flush-top">
-                <h3 className="h2">
-                    <span className="a-icon a-icon__large a-icon__green"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" className="cf-icon-svg"><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm259 284.2L481.4 870.3c-8.2 14.1-22.7 23.4-39 24.8-1.4.1-2.9.2-4.3.2-14.8 0-28.9-6.6-38.4-18L244.4 690.9c-17.9-21-15.4-52.6 5.7-70.5 21-17.9 52.6-15.4 70.5 5.7.2.3.5.5.7.8l109.4 131.4 241.8-418.8c13.6-24 44.2-32.4 68.2-18.8 24 13.6 32.4 44.2 18.8 68.2l-.5.5z"/></svg></span>
-                    Criterion 1: Earning, income, and careers
-                </h3>
+                <h3 className="h2">Criterion 1: Earning, income, and careers</h3>
                 <p className="lead-paragraph">
                     Does the curriculum address grade-level appropriate topics for earning, income, and careers?
                 </p>
