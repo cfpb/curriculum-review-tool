f4fa84061f5a5fb86cf2db6b3cec320c7af57c91
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-04-12 17:10:30 -0400

    Update content for dimension selection bar

--- teachers_digital_platform/crtool/src/js/components/distinctives/DistinctiveMenuBar.js
+++ teachers_digital_platform/crtool/src/js/components/distinctives/DistinctiveMenuBar.js
@@ -4,67 +4,64 @@ import C from "../../constants.js"
 import DistinctiveButton from "../distinctives/DistinctiveButton";
 
 export default class DistinctiveMenuBar extends React.Component {
-  distinctiveClicked() {
-    this.props.distinctiveClicked(this.props.distinctive);
-  }
+    distinctiveClicked() {
+        this.props.distinctiveClicked(this.props.distinctive);
+    }
 
-  render() {
+    render() {
 
-    const distinctiveProps = [
-        {
-          title:"Content",
-          criterionText:"6 criteria",
-          estimateText:"30 minutes",
-          description:"Covers core knowledge and skills in national standards",
-          distinctive:C.CONTENT_PAGE,
-          currentPage:this.props.currentPage,
-          inProgress:this.props.contentInProgress,
-          distinctiveClicked:this.props.distinctiveClicked.bind(this),
-        },
-        {
-          title:"Utility",
-          criterionText:"5 criteria",
-          estimateText:"30 minutes",
-          description:"Supports effective teaching",
-          distinctive:C.UTILITY_PAGE,
-          currentPage:this.props.currentPage,
-          inProgress:this.props.utilityInProgress,
-          distinctiveClicked:this.props.distinctiveClicked.bind(this),
-        },
-        {
-          title:"Quality",
-          criterionText:"4 criteria",
-          estimateText:"30 minutes",
-          description:"Presents clear, accurate, objective information",
-          distinctive:C.QUALITY_PAGE,
-          currentPage:this.props.currentPage,
-          inProgress:this.props.qualityInProgress,
-          distinctiveClicked:this.props.distinctiveClicked.bind(this),
-        },
-        {
-          title:"Efficacy",
-          criterionText:"1 to 3 criteria",
-          estimateText:"30 minutes",
-          description:"Has validated impact on knowledge, skills, or behaviors",
-          distinctive:C.EFFICACY_PAGE,
-          currentPage:this.props.currentPage,
-          inProgress:this.props.efficacyInProgress,
-          distinctiveClicked:this.props.distinctiveClicked.bind(this),
-        }
-      ]
+        const distinctiveProps = [
+            {
+                title:"Content",
+                criterionText:"6 criteria",
+                estimateText:"est. time 60 min.",
+                description:"Covers core knowledge and skills in national standards",
+                distinctive:C.CONTENT_PAGE,
+                currentPage:this.props.currentPage,
+                inProgress:this.props.contentInProgress,
+                distinctiveClicked:this.props.distinctiveClicked.bind(this),
+            },
+            {
+                title:"Utility",
+                criterionText:"5 criteria",
+                estimateText:"est. time 60 min.",
+                description:"Supports effective teaching",
+                distinctive:C.UTILITY_PAGE,
+                currentPage:this.props.currentPage,
+                inProgress:this.props.utilityInProgress,
+                distinctiveClicked:this.props.distinctiveClicked.bind(this),
+            },
+            {
+                title:"Quality",
+                criterionText:"4 criteria",
+                estimateText:"est. time 30 min.",
+                description:"Presents clear, accurate, and objective information",
+                distinctive:C.QUALITY_PAGE,
+                currentPage:this.props.currentPage,
+                inProgress:this.props.qualityInProgress,
+                distinctiveClicked:this.props.distinctiveClicked.bind(this),
+            },
+            {
+                title:"Efficacy",
+                criterionText:"1-3 criteria",
+                estimateText:"est. time 30 min.",
+                description:"Has validated impact on knowledge, skills, or behaviors",
+                distinctive:C.EFFICACY_PAGE,
+                currentPage:this.props.currentPage,
+                inProgress:this.props.efficacyInProgress,
+                distinctiveClicked:this.props.distinctiveClicked.bind(this),
+            }
+        ]
 
-    return (
-        <div>
-          <div className="DistinctivesBlock" >
-            <ul className="o-dimension-selection-bar">
-                {distinctiveProps.map((distinctiveProps, i) => <DistinctiveButton key={i} {...distinctiveProps}/>)}
-            </ul>
-            </div>
-            <br />
-            <div className="u-center">
-                <button className="a-btn a-btn__super">Final Summary</button>
-            </div>
-        </div>
-    );
-  }
+        return (
+            <React.Fragment>
+                <ul className="o-dimension-selection-bar">
+                    {distinctiveProps.map((distinctiveProps, i) => <DistinctiveButton key={i} {...distinctiveProps}/>)}
+                </ul>
+                <div className="u-center">
+                    <button className="a-btn a-btn__super">Final Summary</button>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
