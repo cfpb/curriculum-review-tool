12771850cb816ab709e40f0f78066513d8c2b07d
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-04-13 09:31:54 -0400

    Add appropriate icons to dimension selection bar

--- teachers_digital_platform/crtool/src/js/components/distinctives/DistinctiveButton.js
+++ teachers_digital_platform/crtool/src/js/components/distinctives/DistinctiveButton.js
@@ -11,6 +11,7 @@ export default class DistinctiveButton extends React.Component {
         let statusClassName = "o-dimension-section-bar_button " + this.props.distinctive;
         if (this.props.inProgress === C.STATUS_COMPLETE) {
             statusClassName += " is-complete"
+            this.props.icon = C.SVG_CHECK_ROUND;
         }
         else if (this.props.currentPage === this.props.distinctive) {
             statusClassName += " is-active"
@@ -21,7 +22,7 @@ export default class DistinctiveButton extends React.Component {
                 <button className={statusClassName} onClick={this.handleOnClick.bind(this)}>
                     <div class="o-dimension-section-bar_selector">
                         <span className="o-dimension-section-bar_selector-title">
-                            <span className="a-icon a-icon__large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 651.7 1200" className="cf-icon-svg"><path d="M30 161c-16.5 0-30 13.5-30 30v827.8c0 16.5 13.5 30 30 30h591.7c16.5 0 30-13.5 30-30V343.7L469 161H30zm389.6 60v134.8c0 19.9 16.3 36.2 36.2 36.2h135.9V988.8H60V221h359.6z"/><path d="M123.8 768.6h394.8v50H123.8zM123.8 644h394.8v50H123.8zM123.8 519.5h394.8v50H123.8z"/><circle cx="194" cy="382.3" r="60"/></svg></span>
+                            <span className="a-icon a-icon__large">{this.props.icon}</span>
                             &nbsp;{this.props.title}
                             <span className="o-dimension-section-bar_selector-status">&nbsp;{this.props.inProgress}</span>
                         </span>
