5e7ad62b263e14527804aa5ebc4a4c0e243dedc8
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-04-13 14:36:44 -0400

    Update markup and content for modal dialogs

--- teachers_digital_platform/crtool/src/js/components/dialogs/SaveWorkModal.js
+++ teachers_digital_platform/crtool/src/js/components/dialogs/SaveWorkModal.js
@@ -2,76 +2,76 @@ import React from "react";
 
 export default class SaveWorkModal extends React.Component {
     constructor(props) {
-      super(props);
+        super(props);
 
-      this.setWrapperRef = this.setWrapperRef.bind(this);
-      this.handleClickOutside = this.handleClickOutside.bind(this);
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
     }
 
     componentDidMount() {
-      document.addEventListener('mousedown', this.handleClickOutside);
+        document.addEventListener('mousedown', this.handleClickOutside);
     }
 
     componentWillUnmount() {
-      document.removeEventListener('mousedown', this.handleClickOutside);
+        document.removeEventListener('mousedown', this.handleClickOutside);
     }
 
     /** Set the wrapper ref */
     setWrapperRef(node) {
-    this.wrapperRef = node;
+        this.wrapperRef = node;
     }
 
     /** Alert if clicked on outside of element */
     handleClickOutside(event) {
       if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-        let saveWorkModalDialog = document.getElementById('save-work-modal-dialog');
+        let saveWorkModalDialog = document.getElementById('modal-save-work');
         if (saveWorkModalDialog.classList.contains('o-modal__visible')) {
-          saveWorkModalDialog.classList.remove('o-modal__visible');
+            saveWorkModalDialog.classList.remove('o-modal__visible');
         }
       }
     }
 
     openSaveWorkModalDialog() {
-        let saveWorkModalDialog = document.getElementById('save-work-modal-dialog');
+        let saveWorkModalDialog = document.getElementById('modal-save-work');
         saveWorkModalDialog.classList.add('o-modal__visible');
     }
 
     closeSaveWorkModalDialog() {
-        let saveWorkModalDialog = document.getElementById('save-work-modal-dialog');
+        let saveWorkModalDialog = document.getElementById('modal-save-work');
         saveWorkModalDialog.classList.remove('o-modal__visible');
     }
 
     render() {
         return (
             <div className="l-survey-top">
-                <button type="button" onClick={(e) => {this.openSaveWorkModalDialog()}}>Can I save my work?</button>
+                <button class="a-btn a-btn__link" onClick={(e) => {this.openSaveWorkModalDialog()}}>Can I save my work?</button>
                 <div className="o-modal"
-                    id="save-work-modal-dialog"
+                    id="modal-save-work"
                     aria-hidden="true"
                     role="alertdialog"
-                    aria-labelledby="example-modal-title"
-                    aria-describedby="example-modal-desc">
+                    aria-labelledby="modal-save-work_title"
+                    aria-describedby="modal-save-work_desc">
                     <div className="o-modal_backdrop"></div>
                     <div className="o-modal_container">
-                    <form className="o-modal_content">
-                      <span ref={this.setWrapperRef}>
-                        <div className="o-modal_body">
-                            <button className="o-modal_close a-btn a-btn__link" onClick={(e) => {this.closeSaveWorkModalDialog(); e.preventDefault();}}>
-                                Close
-                                <span className="cf-icon cf-icon-delete-round"></span>
-                            </button>
-                            <h1 id="example-modal-title">Saving your work</h1>
-                            <div id="example-modal-desc">
-                                <p>This tool uses cookies to temporarily save your work. To load answers you’ve already completed, use the same computer and browser, and don't clear your cookies.</p>
-                                <p>To save a permanent copy of your work, please download a summary of each dimension as you complete it. You can download an overall summary of findings after completing any dimension as well.</p>
-                                <p>You can only work on a single curriculum at a time.</p>
-                            </div>
-                        </div>
-                        <div className="o-modal_footer">
-                            <button className="a-btn" onClick={(e) => {this.closeSaveWorkModalDialog(); e.preventDefault();}}>Close</button>
-                        </div>
-                      </span>
-                    </form>
+                        <form className="o-modal_content">
+                            <span ref={this.setWrapperRef}>
+                                <div className="o-modal_body">
+                                    <button className="o-modal_close a-btn a-btn__link" onClick={(e) => {this.closeSaveWorkModalDialog(); e.preventDefault();}}>
+                                        Close
+                                        &nbsp;<span class="a-icon a-icon__large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" class="cf-icon-svg"><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm261.8 692.2c19.4 19.6 19.3 51.3-.3 70.7-19.5 19.3-50.9 19.3-70.4 0L499.6 676.6 308 868.1c-19.6 19.4-51.3 19.3-70.7-.3-19.3-19.5-19.3-50.9 0-70.4l191.6-191.5-191.6-191.6c-19.3-19.8-18.9-51.4.9-70.7 19.4-18.9 50.4-18.9 69.8 0l191.6 191.5 191.5-191.5c19.6-19.4 51.3-19.3 70.7.3 19.3 19.5 19.3 50.9 0 70.4L570.3 605.9l191.5 191.5z"/></svg></span>
+                                    </button>
+                                    <h1 id="modal-save-work_title">Saving your work</h1>
+                                    <div id="modal-save-work_desc">
+                                        <p>This tool uses cookies to <strong>temporarily</strong> save your work. To see answers you’ve already completed, you need to use the same computer and browser, and don’t clear your cookies.</p>
+                                        <p>To save a permanent copy of your work, please print the summary or save it as a PDF for every dimension as you complete it. You can also print or save a summary of the entire review. Learn how to <a href="https://www.consumerfinance.gov/consumer-tools/save-as-pdf-instructions/" target="_blank" rel="noopener noreferrer">save the summary as a PDF</a>.</p>
+                                        <p>You can only work on a single curriculum at a time</p>
+                                    </div>
+                                </div>
+                                <div className="o-modal_footer">
+                                    <button className="a-btn" onClick={(e) => {this.closeSaveWorkModalDialog(); e.preventDefault();}}>Close</button>
+                                </div>
+                            </span>
+                        </form>
                     </div>
                 </div>
             </div>
