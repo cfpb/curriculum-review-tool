24f8193bb7d115cba6e6e80453b9107d004cdb7b
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-04-16 17:03:33 -0400

    Update markup on efficacy summary page

--- teachers_digital_platform/crtool/src/js/components/pages/EfficacySummaryPage.js
+++ teachers_digital_platform/crtool/src/js/components/pages/EfficacySummaryPage.js
@@ -54,9 +54,12 @@ export default class EfficacySummaryPage extends React.Component {
                                 u-mb45
                                 u-mt30" />
                 <div className="l-survey-top">
-                    <button class="a-btn a-btn__link">View or edit responses</button>
+                    <button class="a-btn a-btn__link">
+                        <span className="a-icon a-icon__large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 833.4 1200" class="cf-icon-svg"><path d="M233 203.1c-27.2-27.2-71.8-27.2-99 0L20.4 316.7c-27.2 27.2-27.2 71.8 0 99l89.7 89.7 212.7-212.7-89.8-89.6zM832.6 992.7l-48.6-216c-3.4-15.3-14.9-36.5-26-48.6l-1.8-1.8-121.8-121.9-110.6-110.6-165.7-165.7-54.8 54.8 400 400 21.4-21.4c5.3 6.4 11.9 18.9 13.5 25.6l21.4 95.2-60 60-95.2-21.4c-6.7-1.5-19.2-8.2-25.6-13.5l89.2-89.2-400-400-122.6 122.6 165.7 165.7 110.6 110.6 121.8 121.8 1.8 1.8c12.1 11.1 33.3 22.6 48.6 26l215.9 48.6c2.2.5 4.3.7 6.2.7 12.6.1 19.8-9.4 16.6-23.3z"/></svg></span>&nbsp;
+                        View or edit responses
+                    </button>
                 </div>
-                <h3>Criterion 1: Strength of study (inclusion criteria)</h3>
+                <h3 className="h2">Criterion 1: Strength of study (inclusion criteria)</h3>
                 <p className="u-mb30">Is the study strong? Only strong studies (those that meet rigorous standards) can be used to determine the efficacy of a curriculum. The inclusion criteria will help you determine whether or not a study meets these standards of a strong study.</p>
                 <div className="m-curriculum-status">
                     <ul className="m-list__unstyled
@@ -70,7 +73,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text is-active">
                                         <div><strong>Exceeds</strong></div>
                                         All essential components scored “yes”<br />
                                         At least one beneficial component scored “yes”
@@ -87,7 +90,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Meets</strong></div>
                                         All essential components scored “yes”<br />
                                         None of the beneficial components scored “yes”
@@ -104,7 +107,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Does not meet</strong></div>
                                         One or more essential components scored “no”
                                     </div>
@@ -144,7 +147,7 @@ export default class EfficacySummaryPage extends React.Component {
                 <hr className="hr
                                 u-mb45
                                 u-mt30" />
-                <h3>Criterion 2: Saving and investing</h3>
+                <h3 className="h2">Criterion 2: Saving and investing</h3>
                 <p className="u-mb30">Is there enough evidence (when looking at all the strong studies as a whole) to support the research that this is an effective curriculum?</p>
                 <div className="m-curriculum-status">
                     <ul className="m-list__unstyled
@@ -158,7 +161,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text is-active">
                                         <div><strong>Meets</strong></div>
                                         All essential components scored “yes”
                                     </div>
@@ -174,7 +177,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Does not meet</strong></div>
                                         One or more essential components scored “no”
                                     </div>
@@ -209,7 +212,7 @@ export default class EfficacySummaryPage extends React.Component {
                 <hr className="hr
                                 u-mb45
                                 u-mt30" />
-                <h3>Criterion 3: Impact</h3>
+                <h3 className="h2">Criterion 3: Impact</h3>
                 <p className="u-mb30">Is there enough evidence to support conclusions of consistent, strong, positive impact?</p>
                 <div className="m-curriculum-status">
                     <ul className="m-list__unstyled
@@ -223,7 +226,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text is-active">
                                         <div><strong>Exceeds</strong></div>
                                         All essential components scored “yes”<br />
                                         At least one beneficial component scored “yes”
@@ -240,7 +243,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Meets</strong></div>
                                         All essential components scored “yes”<br />
                                         None of the beneficial components scored “yes”
@@ -257,7 +260,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Does not meet</strong></div>
                                         One or more essential components scored “no”
                                     </div>
@@ -297,13 +300,6 @@ export default class EfficacySummaryPage extends React.Component {
                 <hr className="hr
                                 u-mb45
                                 u-mt30" />
-
-
-
-
-
-
-
                 <div className="o-well u-mb30">
                     <h2>
                         <span className="a-icon a-icon__large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" class="cf-icon-svg"><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm329.9 449.4l-68.7 63.5-69.3 64.1 27.5 138.5 9.1 45.9c10.1 50.9-18.7 71.9-64 46.5l-164.1-91.8-164.1 91.8c-45.3 25.4-74.1 4.4-64-46.5l9.1-45.9 9.1-45.9 18.4-92.6-69.3-64.1-34.4-31.8-34.4-31.8c-38.1-35.3-27.1-69.1 24.4-75.2l93-11 93.8-11.1L441.3 329l19.6-42.5c21.8-47.2 57.4-47.2 79.1 0l19.6 42.5 59.2 128.2 93.8 11.1 93 11c51.4 6.2 62.4 40.1 24.3 75.3z"/></svg></span>
@@ -322,7 +318,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Strong efficacy</strong></div>
                                         All 4 criteria were met, and at least one was exceeded
                                     </div>
@@ -338,7 +334,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text is-active">
                                         <div><strong>Moderate efficacy</strong></div>
                                         All 4 criteria were met
                                     </div>
@@ -354,7 +350,7 @@ export default class EfficacySummaryPage extends React.Component {
                                         <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
                                         <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
                                     </svg>
-                                    <div>
+                                    <div className="m-form-field_radio-text">
                                         <div><strong>Limited efficacy</strong></div>
                                         At least one of the criteria was not met
                                     </div>
