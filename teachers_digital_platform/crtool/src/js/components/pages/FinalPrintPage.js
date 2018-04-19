8365e60bc8b0e1fee0189eb792dbba808b52d009
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-04-19 14:56:05 -0400

    Update content and markup on final pring page

--- teachers_digital_platform/crtool/src/js/components/pages/FinalPrintPage.js
+++ teachers_digital_platform/crtool/src/js/components/pages/FinalPrintPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import C from "../../constants";
-import FinalSummaryButton from "../buttons/FinalSummaryButton";
-import PrintIntroComponent from "./partial_pages/PrintIntroComponent";
-import StartOverModal from "../dialogs/StartOverModal";
-import CriterionComponent from "../criterion/CriterionComponent";
 import ContentSwitchComponent from "./partial_pages/ContentSwitchComponent";
+import CriterionComponent from "../criterion/CriterionComponent";
+import PrintIntroComponent from "./partial_pages/PrintIntroComponent";
+import SvgIcon from "../svgs/SvgIcon";
+import { EfficacyContent } from "../../content_data/efficacyContent";
+import { QualityContent } from "../../content_data/qualityContent";
 import { UtilityContent } from "../../content_data/utilityContent";
 
 export default class FinalPrintPage extends React.Component {
@@ -17,63 +17,208 @@ export default class FinalPrintPage extends React.Component {
     render() {
         return (
             <React.Fragment>
-            <PrintIntroComponent {...this.props} />
-            <div className="block
-                block__flush-top
-                block__padded-bottom
-                block__border-bottom
-                u-page-break-before">
-                <h2 className="h1" >
-                    <span className="a-icon a-icon__large"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1200" className="cf-icon-svg"><path d="M379.9 493.6c-.7 0-1.5 0-2.2-.1-.7 0-1.5-.1-2.2-.2s-1.5-.2-2.2-.3c-.7-.1-1.5-.2-2.2-.4-.7-.1-1.4-.3-2.2-.5l-2.1-.6c-.7-.2-1.4-.4-2.1-.7-.7-.2-1.4-.5-2.1-.8s-1.3-.6-2-.9-1.3-.6-2-1c-.6-.3-1.3-.7-1.9-1.1s-1.3-.8-1.9-1.2c-.6-.4-1.2-.8-1.8-1.3-.6-.4-1.2-.9-1.8-1.4-.6-.5-1.1-.9-1.7-1.4-.5-.5-1.1-1-1.6-1.5s-1-1.1-1.5-1.6-1-1.1-1.4-1.7c-.5-.6-.9-1.2-1.4-1.7-.4-.6-.9-1.2-1.3-1.8-.4-.6-.8-1.2-1.2-1.9-.4-.6-.7-1.3-1.1-1.9-.3-.6-.7-1.3-1-2s-.6-1.3-.9-2-.5-1.4-.8-2c-.2-.7-.5-1.4-.7-2.1l-.6-2.1c-.2-.7-.3-1.4-.5-2.2-.1-.7-.3-1.4-.4-2.2-.1-.7-.2-1.5-.3-2.2-.1-.7-.1-1.5-.2-2.2 0-.7-.1-1.5-.1-2.2s0-1.5.1-2.2c0-.7.1-1.5.2-2.2s.2-1.5.3-2.2c.1-.7.2-1.4.4-2.2.2-.7.3-1.4.5-2.1l.6-2.1c.2-.7.5-1.4.7-2.1s.5-1.4.8-2.1.6-1.4.9-2c.3-.7.7-1.3 1-2 .3-.6.7-1.3 1.1-1.9s.8-1.3 1.2-1.9c.4-.6.8-1.2 1.3-1.8.4-.6.9-1.2 1.4-1.7.5-.6 1-1.1 1.4-1.7.5-.5 1-1.1 1.5-1.6s1.1-1 1.6-1.5 1.1-1 1.7-1.4c.6-.5 1.2-.9 1.8-1.4.6-.4 1.2-.9 1.8-1.3.6-.4 1.2-.8 1.9-1.2.6-.4 1.3-.7 1.9-1.1.6-.3 1.3-.7 2-1s1.3-.6 2-.9 1.4-.5 2.1-.8c.7-.2 1.4-.5 2.1-.7l2.1-.6c.7-.2 1.4-.3 2.2-.5.7-.1 1.5-.3 2.2-.4s1.5-.2 2.2-.3c.7-.1 1.5-.1 2.2-.2 1.5-.1 3-.1 4.4 0 .7 0 1.5.1 2.2.2s1.5.2 2.2.3c.7.1 1.5.2 2.2.4.7.1 1.4.3 2.2.5l2.1.6c.7.2 1.4.5 2.1.7s1.4.5 2 .8c.7.3 1.4.6 2 .9.7.3 1.3.6 2 1 .6.3 1.3.7 1.9 1.1s1.2.8 1.9 1.2c.6.4 1.2.8 1.8 1.3.6.4 1.2.9 1.8 1.4.6.5 1.1 1 1.7 1.4.5.5 1.1 1 1.6 1.5s1 1.1 1.5 1.6 1 1.1 1.5 1.7.9 1.1 1.4 1.7c.4.6.9 1.2 1.3 1.8.4.6.8 1.2 1.2 1.9.4.6.7 1.3 1.1 1.9.3.6.7 1.3 1 2s.6 1.3.9 2 .5 1.4.8 2.1c.2.7.5 1.4.7 2.1l.6 2.1c.2.7.3 1.4.5 2.1.1.7.3 1.5.4 2.2s.2 1.5.3 2.2c.1.7.1 1.5.2 2.2 0 .7.1 1.5.1 2.2s0 1.5-.1 2.2c0 .7-.1 1.5-.2 2.2s-.2 1.5-.3 2.2c-.1.7-.2 1.5-.4 2.2-.1.7-.3 1.4-.5 2.2l-.6 2.1c-.2.7-.4 1.4-.7 2.1-.2.7-.5 1.4-.8 2-.3.7-.6 1.4-.9 2-.3.7-.6 1.3-1 2-.3.7-.7 1.3-1.1 1.9s-.8 1.3-1.2 1.9c-.4.6-.8 1.2-1.3 1.8-.4.6-.9 1.2-1.4 1.7-.5.6-1 1.1-1.5 1.7s-1 1.1-1.5 1.6-1.1 1-1.6 1.5-1.1 1-1.7 1.4c-.6.5-1.2.9-1.8 1.4-.6.4-1.2.9-1.8 1.3-.6.4-1.2.8-1.9 1.2-.6.4-1.3.7-1.9 1.1-.7.3-1.3.7-2 1s-1.3.6-2 .9-1.4.5-2 .8c-.7.2-1.4.5-2.1.7l-2.1.6c-.7.2-1.4.3-2.2.5-.7.1-1.5.3-2.2.4s-1.5.2-2.2.3c-.7.1-1.5.1-2.2.2-.7.1-1.5.1-2.2.1zM331.6 681.2h329.3v40H331.6z"/><circle cx="380.1" cy="448.8" r="45.2"/><path d="M331.6 782.8h329.3v40H331.6zM331.6 579.5h329.3v40H331.6z"/><path d="M500 105.2c-276.1 0-500 223.9-500 500s223.9 500 500 500 500-223.9 500-500-223.9-500-500-500zm230.8 794.5c0 11-9 20-20 20H290.3c-11 0-20-9-20-20V312.4c0-11 9-20 20-20l239.2.2v166.3c0 16.8 13.8 30.6 30.6 30.6h171.3l-.6 410.2zm.6-456.5H575.8V292.7l155.5 150.1v.4h.1z"/></svg></span>
-                    Content
-                </h2>
-                <p className="lead-paragraph">
-                    This dimension assesses whether the curriculum content helps students develop knowledge, skills, and behaviors that are important for financial capability. Evaluation criteria are based on research and major national and state education standards.
-                </p>
-                <h3 className="h4">This dimension has <em>essential</em> and <em>beneficial</em> components.</h3>
-                <p>
-                    <b><em>Essential components</em></b> 
-                    have been shown to positively impact student learning.
-                    <br /><b><em>Beneficial components</em></b> 
-                    hold promise for positive impact on student learning, but may be more relevant and useful for some reviewers. Beneficial components are marked with a note; all others are essential. You must answer all components.
-                </p>
-                <ContentSwitchComponent {...this.props} />
-            </div>
-
-            <div>
-                <h2> Utility </h2>
-                {UtilityContent.criterion.map((criterion, i) => <CriterionComponent key={i} criterionData={UtilityContent.criterion[i]} {...this.props} />)}
-            </div>
-
-            <div>
-                <h2> Quality </h2>
-            </div>
-
-            <div>
-                <h2> Efficacy </h2>
-            </div>
-
-            <div>
-                
-                <br /><br />
-
-                <FinalSummaryButton handleFinalSummaryButtonClick={this.handleFinalSummaryButtonClick.bind(this)}
-                    contentInProgress={this.props.contentInProgress}
-                    utilityInProgress={this.props.utilityInProgress}
-                    qualityInProgress={this.props.qualityInProgress}
-                    efficacyInProgress={this.props.efficacyInProgress} />
-
-                <br /><br />
-
-
-                <div className="u-center">
-                    <button className="a-btn a-btn__super" onClick={(e) => {this.props.distinctiveClicked(C.START_PAGE); e.preventDefault();}}>Back</button>
+                <PrintIntroComponent {...this.props} />
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom">
+                    <h2>
+                        <SvgIcon
+                            icon="document-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Content score
+                    </h2>
+                    <p className="u-mb30">How the curriculum meets the criteria for content:</p>
+                    <ul className="m-list__unstyled">
+                        <li className="u-mb30">
+                            <div className="m-form-field
+                                            m-form-field__radio
+                                            m-form-field__display">
+                                <div className="a-label">
+                                    <svg className="m-form-field_radio-icon" viewBox="0 0 22 22">
+                                        <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
+                                        <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
+                                    </svg>
+                                    <div className="m-form-field_radio-text">
+                                        <div><strong>Strong content</strong></div>
+                                        All 6 criteria were met, and at least one was exceeded
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="u-mb30">
+                            <div className="m-form-field
+                                            m-form-field__radio
+                                            m-form-field__display">
+                                <div className="a-label">
+                                    <svg className="m-form-field_radio-icon is-active" viewBox="0 0 22 22">
+                                        <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
+                                        <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
+                                    </svg>
+                                    <div className="m-form-field_radio-text is-active">
+                                        <div><strong>Moderate content</strong></div>
+                                        All 6 criteria were met
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li className="u-mb30">
+                            <div className="m-form-field
+                                            m-form-field__radio
+                                            m-form-field__display">
+                                <div className="a-label">
+                                    <svg className="m-form-field_radio-icon" viewBox="0 0 22 22">
+                                        <circle cx="11" cy="11" r="10" className="m-form-field_radio-icon-stroke"></circle>
+                                        <circle cx="11" cy="11" r="7" className="m-form-field_radio-icon-fill"></circle>
+                                    </svg>
+                                    <div className="m-form-field_radio-text">
+                                        <div><strong>Limited content</strong></div>
+                                        At least one criterion was not met
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div className="m-form-field u-mb30">
+                        <label className="a-label a-label__heading">
+                            Assets
+                            &nbsp;<small className="a-label_helper">(optional)</small>
+                        </label>
+                        <p><em>No information provided</em></p>
+                    </div>
+                    <div className="m-form-field u-mb30">
+                        <label className="a-label a-label__heading">
+                            Gaps
+                            &nbsp;<small className="a-label_helper">(optional)</small>
+                        </label>
+                        <p>These are the notes I previously entered. I can no longer edit these notes, which is why there’s not a text field box around me.</p>
+                    </div>
+                    <div className="m-form-field">
+                        <label className="a-label a-label__heading">
+                            Overall notes
+                            &nbsp;<small className="a-label_helper">(optional)</small>
+                        </label>
+                        <p>These are the notes I previously entered. I can no longer edit these notes, which is why there’s not a text field box around me.</p>
+                    </div>
                 </div>
-
-                <br /><br />
-
-                <StartOverModal clearLocalStorage={(e) => {this.props.clearLocalStorage(); e.preventDefault();}}/>
-            </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom">
+                    <h2>
+                        <SvgIcon
+                            icon="settings-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Utility score
+                    </h2>
+                    <p><em>Dimension not reviewed</em></p>
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom">
+                    <h2>
+                        <SvgIcon
+                            icon="star-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Quality score
+                    </h2>
+                    <p><em>Dimension not reviewed</em></p>
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom">
+                    <h2>
+                        <SvgIcon
+                            icon="credit-report-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Efficacy score
+                    </h2>
+                    <p><em>Dimension not reviewed</em></p>
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom">
+                    <h2>
+                        Key takeaways
+                        &nbsp;<small className="a-label_helper">(optional)</small>
+                    </h2>
+                    <p>These are my final thoughts that I entered on the final summary page. I am no longer editable now that I’m on the print page. To save on ink, I’ve dropped by gray highlight box. I’m thoughtful.</p>
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom
+                                u-page-break-before">
+                    <h2 className="h1">
+                        <SvgIcon
+                            icon="document-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Content
+                    </h2>
+                    <p className="lead-paragraph">
+                        This dimension assesses whether the curriculum content helps students develop knowledge, skills, and behaviors that are important for financial capability.
+                    </p>
+                    <ContentSwitchComponent {...this.props} />
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom
+                                u-page-break-before">
+                    <h2 className="h1">
+                        <SvgIcon
+                            icon="settings-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Utility
+                    </h2>
+                    <p className="lead-paragraph">
+                        The utility dimension evaluates the supports for using the curriculum. Such supports include guidance for teachers, materials that facilitate strong and effective instruction, and assessments to measure student mastery of skills and knowledge.
+                    </p>
+                    {UtilityContent.criterion.map((criterion, i) => <CriterionComponent key={i} criterionData={UtilityContent.criterion[i]} {...this.props} />)}
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom
+                                u-page-break-before">
+                    <h2 className="h1">
+                        <SvgIcon
+                            icon="star-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Quality
+                    </h2>
+                    <p className="lead-paragraph">
+                        The quality dimension assesses whether curriculum materials are clear, accurate, and objective and how easy the materials are for teachers and students to access.
+                    </p>
+                    {QualityContent.criterion.map((criterion, i) => <CriterionComponent key={i} criterionData={QualityContent.criterion[i]} {...this.props} />)}
+                </div>
+                <div className="block
+                                block__flush-top
+                                block__padded-bottom
+                                block__border-bottom
+                                u-page-break-before">
+                    <h2 className="h1">
+                        <SvgIcon
+                            icon="credit-report-round"
+                            isLarge="true"
+                            hasSpaceAfter="true" />
+                        Efficacy
+                    </h2>
+                    <p className="lead-paragraph">
+                        The efficacy dimension assesses the measurable impact the curriculum has had on students by looking at high-quality studies that have been done about its effectiveness.
+                    </p>
+                    {EfficacyContent.criterion.map((criterion, i) => <CriterionComponent key={i} criterionData={EfficacyContent.criterion[i]} {...this.props} />)}
+                </div>
             </React.Fragment>
         );
     }
