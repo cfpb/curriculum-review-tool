298d0dd1a65655dcaf14f995b7651a195cb673d7
Allen Tuggle <allen.tuggle@cfpb.gov>
2018-04-27 13:34:21 -0400

    Added back summary style DimensionInformation

--- teachers_digital_platform/crtool/src/js/components/common/DimensionInformation.js
+++ teachers_digital_platform/crtool/src/js/components/common/DimensionInformation.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import C from "../../business.logic/constants";
+
 export default class DimensionInformation extends React.Component {
     renderReviewedOnDate() {
         if (this.props.reviewedOnDate !== undefined) {
@@ -7,22 +9,38 @@ export default class DimensionInformation extends React.Component {
         }
         return this.props.distinctiveCompletedDate[this.props.currentPage]
     }
+    
     render() {
-        return (
-            <React.Fragment>
-                <div className="block
-                                block__flush-top
-                                block__padded-bottom">
-                    <div className="h5">CFPB curriculum review tool</div>
-                    <h1>{this.props.dimensionName} summary for {this.props.curriculumTitle}</h1>
-                    <p className="lead-paragraph u-mb30">
-                        {this.props.dimensionSummary}
-                    </p>
-                    <p><strong>Grade range:</strong> {this.props.gradeRange}</p>
+        if (this.props.currentPrintButton !== C.START_PAGE) {
+            return (
+                <React.Fragment>
+                    <div className="block
+                                    block__flush-top
+                                    block__padded-bottom
+                                    block__border-top">
+                        <div className="h5">CFPB curriculum review tool</div>
+                        <h1>{this.props.dimensionName} summary for {this.props.curriculumTitle}</h1>
+                        <p className="lead-paragraph u-mb30">
+                            {this.props.dimensionSummary}
+                        </p>
+                        <p><strong>Grade range:</strong> {this.props.gradeRange}</p>
+                        <p><strong>Date of publication:</strong> {this.props.publicationDate}</p>
+                        <p><strong>Reviewed on:</strong> {this.renderReviewedOnDate()}</p>
+                    </div>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment>
+                    <hr class="hr u-mb45 u-mt30" />
+                    <h2>Curriculum information</h2>
+                    <p><strong>Curriculum title:</strong> {this.props.dimensionName}</p>
                     <p><strong>Date of publication:</strong> {this.props.publicationDate}</p>
+                    <p><strong>Grade range:</strong> {this.props.gradeRange}</p>
                     <p><strong>Reviewed on:</strong> {this.renderReviewedOnDate()}</p>
-                </div>
-            </React.Fragment>
-        );
+                    <hr class="hr u-mb45 u-mt30" />
+                </React.Fragment>
+            );
+        } 
     }
 }
