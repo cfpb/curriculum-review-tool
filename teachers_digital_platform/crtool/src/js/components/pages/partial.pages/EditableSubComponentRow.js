93ca3796cedd5e9ef4e914c5a997cff6616c37e2
Russell Stoll <russell.stoll@cfpb.gov>
2018-05-08 16:11:39 -0400

    Efficacy EditableSubComponentRow and QualityCriterionPage edits

--- teachers_digital_platform/crtool/src/js/components/pages/partial.pages/EditableSubComponentRow.js
+++ teachers_digital_platform/crtool/src/js/components/pages/partial.pages/EditableSubComponentRow.js
@@ -14,38 +14,41 @@ export default class EditableSubComponentRow extends React.Component {
 
     render() {
         return (
+            <fieldset className="o-survey_fieldset">
+                <div className="o-survey_component">
+                    <div class="o-survey_question">
+                        <legend className="o-survey_legend">
+                            <p>{this.props.componentText}</p>
+                            {this.showBeneficialText()}
+                        </legend>
+                    </div>
+                    <div className="o-survey_answer">
+                        <RadioButtonEditable
+                            uniqueId="a"
+                            radioText="Yes"
+                            showButton="true"
+                            isChecked={this.props.criterionAnswers[this.props.currentCriterionRefId] === 'yes'}
+                            radioButtonClicked={(e) => {this.props.criterionAnswerChanged(this.props.currentCriterionRefId, 'yes');}}
+                            {...this.props} />
 
-            <div className="o-survey_component">
-                <div class="o-survey_question">
-                    <p>{this.props.componentText}</p>
-                    {this.showBeneficialText()}
+                        <RadioButtonEditable
+                            uniqueId="b"
+                            radioText="No"
+                            showButton="true"
+                            isChecked={this.props.criterionAnswers[this.props.currentCriterionRefId] === 'no'}
+                            radioButtonClicked={(e) => {this.props.criterionAnswerChanged(this.props.currentCriterionRefId, 'no');}}
+                            {...this.props} />
+
+                        <RadioButtonEditable
+                            uniqueId="c"
+                            radioText="NA"
+                            showButton={this.props.showNaButton}
+                            isChecked={this.props.criterionAnswers[this.props.currentCriterionRefId] === 'na'}
+                            radioButtonClicked={(e) => {this.props.criterionAnswerChanged(this.props.currentCriterionRefId, 'na');}}
+                            {...this.props} />
+                    </div>
                 </div>
-                <div className="o-survey_answer">
-                    <RadioButtonEditable
-                        uniqueId="a"
-                        radioText="Yes"
-                        showButton="true"
-                        isChecked={this.props.criterionAnswers[this.props.currentCriterionRefId] === 'yes'}
-                        radioButtonClicked={(e) => {this.props.criterionAnswerChanged(this.props.currentCriterionRefId, 'yes');}}
-                        {...this.props} />
-
-                    <RadioButtonEditable
-                        uniqueId="b"
-                        radioText="No"
-                        showButton="true"
-                        isChecked={this.props.criterionAnswers[this.props.currentCriterionRefId] === 'no'}
-                        radioButtonClicked={(e) => {this.props.criterionAnswerChanged(this.props.currentCriterionRefId, 'no');}}
-                        {...this.props} />
-
-                    <RadioButtonEditable
-                        uniqueId="c"
-                        radioText="NA"
-                        showButton={this.props.showNaButton}
-                        isChecked={this.props.criterionAnswers[this.props.currentCriterionRefId] === 'na'}
-                        radioButtonClicked={(e) => {this.props.criterionAnswerChanged(this.props.currentCriterionRefId, 'na');}}
-                        {...this.props} />
-                </div>
-            </div>
+            </fieldset>
         );
     }
 }
