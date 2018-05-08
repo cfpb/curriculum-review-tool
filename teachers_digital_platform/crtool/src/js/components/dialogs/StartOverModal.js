bf026201697c1dce922eecde7fbb2301dd18f3be
Allen Tuggle <allen.tuggle@cfpb.gov>
2018-05-08 13:54:59 -0400

    GH#63: Converted modal dialogs to use react-modal with accessibility

--- teachers_digital_platform/crtool/src/js/components/dialogs/StartOverModal.js
+++ teachers_digital_platform/crtool/src/js/components/dialogs/StartOverModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Modal from "react-modal";
 
 import C from "../../business.logic/constants";
 import SvgIcon from "../svgs/SvgIcon";
@@ -6,6 +7,10 @@ import SvgIcon from "../svgs/SvgIcon";
 export default class StartOverModal extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            modalIsOpen: false
+        };
 
         this.setWrapperRef = this.setWrapperRef.bind(this);
         this.handleStartOverClickOutside = this.handleStartOverClickOutside.bind(this);
@@ -29,8 +34,7 @@ export default class StartOverModal extends React.Component {
     /* Click Outside setup */
     handleStartOverClickOutside(event) {
         if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-            let startOverModalDialog = document.getElementById('modal-start-over');
-            startOverModalDialog.classList.remove('o-modal__visible');
+            this.setState({modalIsOpen: false});
         }
     }
 
@@ -41,14 +45,12 @@ export default class StartOverModal extends React.Component {
 
     /* Modal specific open dialog */
     openstartOverModalDialog() {
-        let startOverModalDialog = document.getElementById('modal-start-over');
-        startOverModalDialog.classList.add('o-modal__visible');
+        this.setState({modalIsOpen: true});
     }
 
     /* Modal specific close dialog */
     closestartOverModalDialog() {
-        let startOverModalDialog = document.getElementById('modal-start-over');
-        startOverModalDialog.classList.remove('o-modal__visible');
+        this.setState({modalIsOpen: false});
     }
 
     render() {
@@ -57,37 +59,42 @@ export default class StartOverModal extends React.Component {
             <button className="a-btn a-btn__link" onClick={(e) => {this.openstartOverModalDialog();}}>
                 Start over with a new review
             </button>
-            <div className="o-modal"
-                id="modal-start-over"
-                aria-hidden="true"
-                role="alertdialog"
-                aria-labelledby="modal-start-over_title"
-                aria-describedby="modal-start-over_desc">
-                <div className="o-modal_backdrop"></div>
-                <div className="o-modal_container">
-                    <form className="o-modal_content" ref={this.setWrapperRef}>
-                        <div className="o-modal_body">
-                            <button className="o-modal_close a-btn a-btn__link" onClick={(e) => {this.closestartOverModalDialog(); e.preventDefault();}}>
-                                Close
-                                <SvgIcon
-                                    icon="x-round"
-                                    isLarge="true"
-                                    hasSpaceBefore="true" />
-                            </button>
-                            <h1 id="modal-start-over_title" className="h3">Starting over</h1>
-                            <div id="modal-start-over_desc">
-                                <p>Starting a new review will erase your answers for all dimensions. Are you sure you want to start a new review?</p>
+            <Modal
+                isOpen={this.state.modalIsOpen}
+                className="o-modal_container"
+                onRequestClose={this.closeModal} >
+                <div className="o-modal o-modal__visible"
+                    id="modal-start-over"
+                    aria-hidden="true"
+                    role="alertdialog"
+                    aria-labelledby="modal-start-over_title"
+                    aria-describedby="modal-start-over_desc">
+                    <div className="o-modal_backdrop"></div>
+                    <div className="o-modal_container">
+                        <form className="o-modal_content" ref={this.setWrapperRef}>
+                            <div className="o-modal_body">
+                                <button className="o-modal_close a-btn a-btn__link" onClick={(e) => {this.closestartOverModalDialog(); e.preventDefault();}}>
+                                    Close
+                                    <SvgIcon
+                                        icon="x-round"
+                                        isLarge="true"
+                                        hasSpaceBefore="true" />
+                                </button>
+                                <h1 id="modal-start-over_title" className="h3">Starting over</h1>
+                                <div id="modal-start-over_desc">
+                                    <p>Starting a new review will erase your answers for all dimensions. Are you sure you want to start a new review?</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="o-modal_footer">
-                            <div className="m-btn-group m-btn-group__wide">
-                                <button className="a-btn" onClick={(e) => {this.clearLocalStorage()}} formAction={C.START_PAGE_RELATIVE_URL} >Yes</button>
-                                <button className="a-btn a-btn__link" onClick={(e) => {this.closestartOverModalDialog(); e.preventDefault();}}>No, return to current review</button>
+                            <div className="o-modal_footer">
+                                <div className="m-btn-group m-btn-group__wide">
+                                    <button className="a-btn" onClick={(e) => {this.clearLocalStorage()}} formAction={C.START_PAGE_RELATIVE_URL} >Yes</button>
+                                    <button className="a-btn a-btn__link" onClick={(e) => {this.closestartOverModalDialog(); e.preventDefault();}}>No, return to current review</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            </Modal>
         </React.Fragment>
         );
     }
