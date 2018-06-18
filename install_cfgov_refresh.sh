1268d57a58cccd0bfd0d0a10052a48d0702255ac
Will Barton <william.barton@cfpb.gov>
2018-06-18 16:29:56 -0400

    Clone with --depth 1

--- install_cfgov_refresh.sh
+++ install_cfgov_refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git clone https://github.com/cfpb/cfgov-refresh
+git clone --depth 1 https://github.com/cfpb/cfgov-refresh
 cd cfgov-refresh
 pip install -r requirements/wagtail.txt
 pip install -r requirements/libraries.txt
