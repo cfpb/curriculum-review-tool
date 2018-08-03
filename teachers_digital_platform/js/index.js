b08c5e63fe14696d9a8ceffd94d060371ce28715
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-08-03 16:28:56 -0400

    Add cf-expandables JS

---
+++ teachers_digital_platform/js/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Your app's JavaScript may look something like this -- or it may not.
+//
+// const module1 = require( './module1.js' );
+// const module2 = require( './module2.js' );
+//
+// const app = {
+//   init: () => {
+//     module1.init();
+//     module2.init();
+//   }
+// };
+//
+// app.init();
+
+const cfExpandables = require( 'cf-expandables/src/Expandable' );
+
+cfExpandables.init();
