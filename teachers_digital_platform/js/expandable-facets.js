07c1f29c0a8a8e417b5297f6f4a7a7aa3a586d9a
Russell Stoll <russell.stoll@cfpb.gov>
2018-08-31 16:09:42 -0400

    Modifying the expandable-facets.js file.

--- teachers_digital_platform/js/expandable-facets.js
+++ teachers_digital_platform/js/expandable-facets.js
@@ -13,7 +13,118 @@ const contains = domClassList.contains;
 const removeClass = domClassList.removeClass;
 const closest = require( 'cf-atomic-component/src/utilities/dom-closest' ).closest;
 
+const Events = require( 'cf-atomic-component/src/mixins/Events.js' );
+const Organism = require( 'cf-atomic-component/src/components/Organism' );
+const ExpandableTransition = require( 'cf-expandables/src/ExpandableTransition' );
+
+const ExpandableFacets = Organism.extend( {
+  ui: {
+    base:    '.o-expandable-facets',
+    target:  '.o-expandable-facets_target',
+    content: '.o-expandable-facets_content',
+    header:  '.o-expandable_header'
+  },
+
+  classes: {
+    targetExpanded:  'is-open',
+    targetCollapsed: 'is-closed',
+    group:           'o-expandable-group',
+    groupAccordion:  'o-expandable-group__accordion'
+  },
+
+  events: {
+    'click .o-expandable-facets_target': 'expandableClickHandler'
+  },
+
+  transition:       null,
+  isAccordionGroup: false,
+  activeAccordion:  false,
+
+  initialize:             initialize,
+  expandableClickHandler: expandableClickHandler,
+  toggleTargetState:      toggleTargetState
+} );
+
+/**
+ * Initialize a new expandable.
+ */
+function initialize() {
+  const transition = new ExpandableTransition(
+    this.ui.content
+  );
+  this.transition = transition.init();
+
+  if ( contains( this.ui.content, ExpandableTransition.CLASSES.EXPANDED ) ) {
+    addClass( this.ui.target, this.classes.targetExpanded );
+  } else {
+    addClass( this.ui.target, this.classes.targetCollapsed );
+  }
+
+  const expandableGroup = closest(
+    this.ui.target, '.' + this.classes.group
+  );
+
+  this.isAccordionGroup = expandableGroup !== null &&
+    contains( expandableGroup, this.classes.groupAccordion );
+
+  if ( this.isAccordionGroup ) {
+    Events.on(
+      'accordionActivated',
+      _accordionActivatedHandler.bind( this )
+    );
+  }
+}
+
+/**
+ * Event handler for when an accordion is activated
+ */
+function _accordionActivatedHandler() {
+  if ( this.activeAccordion ) {
+    this.transition.toggleExpandable();
+    this.toggleTargetState( this.ui.target );
+    this.activeAccordion = false;
+  }
+}
+
+/**
+ * Event handler for when an expandable is clicked.
+ */
+function expandableClickHandler() {
+  this.transition.toggleExpandable();
+  this.toggleTargetState( this.ui.target );
+
+  if ( this.isAccordionGroup ) {
+    if ( this.activeAccordion ) {
+      this.activeAccordion = false;
+    } else {
+      Events.trigger( 'accordionActivated', { target: this } );
+      this.activeAccordion = true;
+    }
+  }
+}
+
+/**
+ * Toggle an expandable to open or closed.
+ * @param {HTMLNode} element - The expandable target HTML DOM element.
+ */
+function toggleTargetState( element ) {
+  if ( contains( element, this.classes.targetExpanded ) ) {
+    addClass( this.ui.target, this.classes.targetCollapsed );
+    removeClass( this.ui.target, this.classes.targetExpanded );
+  } else {
+    addClass( this.ui.target, this.classes.targetExpanded );
+    removeClass( this.ui.target, this.classes.targetCollapsed );
+  }
+}
+
+module.exports = ExpandableFacets;
+console.log('Russ');
+
 /**
  * Find .o-expandable-facets, add `is-open` class.
  * Find .o-expandable-facets_target and add a click handler to toggle classes on .o-expandable-facets between `is-open` and `is-closed`.
  */
+
+
+
+
