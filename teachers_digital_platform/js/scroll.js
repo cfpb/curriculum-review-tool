7841e4f7200c4dd7a1493400be81127c1c284bb0
Dan Mouyard <daniel.mouyard@cfpb.gov>
2018-09-10 12:31:15 -0400

    Fix lint errors in scroll.js

--- teachers_digital_platform/js/scroll.js
+++ teachers_digital_platform/js/scroll.js
@@ -3,39 +3,38 @@
 const smoothscroll = require( 'smoothscroll-polyfill' );
 
 const scroll = {
-    init: () => {
-        const jumplinks = document.querySelectorAll( '[data-scroll]' );
+  init: () => {
+    const jumplinks = document.querySelectorAll( '[data-scroll]' );
 
-        smoothscroll.polyfill();
+    smoothscroll.polyfill();
 
-        jumplinks.forEach( function( jumplink ) {
-            jumplink.addEventListener( 'click', function( event ) {
-                const target = document.querySelector( jumplink.hash );
+    jumplinks.forEach( function( jumplink ) {
+      jumplink.addEventListener( 'click', function( event ) {
+        const target = document.querySelector( jumplink.hash );
 
-                // Disable default browser behavior.
-                event.preventDefault();
+        // Disable default browser behavior.
+        event.preventDefault();
 
-                // Scroll smoothly to the target.
-                target.scrollIntoView({ behavior: 'smooth' });
+        // Scroll smoothly to the target.
+        target.scrollIntoView( { behavior: 'smooth' } );
 
-                // Update url hash.
-                if ( history.pushState ) {
-                    history.pushState( null, null, jumplink.hash );
-                }
-                else {
-                    location.hash = jumplink.hash;
-                }
+        // Update url hash.
+        if ( history.pushState ) {
+          history.pushState( null, null, jumplink.hash );
+        } else {
+          location.hash = jumplink.hash;
+        }
 
-                // Wait half a second for scrolling to finish.
-                setTimeout( function () {
+        // Wait half a second for scrolling to finish.
+        setTimeout( function() {
 
-                    // Make sure focus is set to the target.
-                    target.setAttribute( 'tabindex', '-1' );
-                    target.focus({ preventScroll: true });
-                }, 500);
-            }, false);
-        });
-    }
+          // Make sure focus is set to the target.
+          target.setAttribute( 'tabindex', '-1' );
+          target.focus( { preventScroll: true } );
+        }, 500 );
+      }, false );
+    } );
+  }
 };
 
 module.exports = scroll;
