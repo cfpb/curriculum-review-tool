63ae28ce2d7a0d7ed80c721bc200856c50bfd6b8
Norman Bucknor <nbucknor@forumone.com>
2018-09-25 12:53:21 -0400

    [TDP] Fix clear filter tags in IE11

--- teachers_digital_platform/js/search.js
+++ teachers_digital_platform/js/search.js
@@ -64,15 +64,28 @@ function clearFilter( event ) {
     return;
   }
   target = closest( event.target, '.a-tag' );
-  const checkbox = find( `${ target.dataset.value }` );
+  const checkbox = find( `${ target.getAttribute( 'data-value' ) }` );
   // Remove the filter tag
-  target.remove();
+  removeTag( target );
   // Uncheck the filter checkbox
   checkbox.checked = false;
   if ( event instanceof Event ) {
     handleFilter( event, checkbox );
   }
 }
+
+/**
+ * Remove a filter tag from the search results page.
+ * node.remove() isn't supported by IE so we have to removeChild();
+ *
+ * @param {Node} tag Filter tag HTML element
+ */
+function removeTag( tag ) {
+  if ( tag.parentNode !== null ) {
+    tag.parentNode.removeChild( tag );
+  }
+}
+
 
 /**
  * Remove all filters from the search results page.
