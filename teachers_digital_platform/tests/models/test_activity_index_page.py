f53d14fd8d2049f4305f922e5c5caaf03c78bd01
Norman Bucknor <nbucknor@forumone.com>
2018-10-03 14:15:20 -0400

    Updating unit tests to work with text_introduction streamfield and initial linting fixes

--- teachers_digital_platform/tests/models/test_activity_index_page.py
+++ teachers_digital_platform/tests/models/test_activity_index_page.py
@@ -1,7 +1,15 @@
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.models import Site
+from django.test import RequestFactory, TestCase, override_settings
+from django.http import Http404, HttpRequest, HttpResponse
+
+from wagtail.wagtailcore.blocks import StreamValue
+from wagtail.wagtailcore.models import Page, Site
 from wagtail.wagtaildocs.models import Document
 from wagtail.tests.utils import WagtailPageTests
+from v1.tests.wagtail_pages.helpers import publish_page
+
+
+from scripts import _atomic_helpers as atomic
+
 from teachers_digital_platform.models import ActivityIndexPage
 from teachers_digital_platform.models import ActivityPage
 from teachers_digital_platform.models import (
@@ -9,13 +17,12 @@ from teachers_digital_platform.models import (
     ActivityStudentCharacteristics, ActivityType, ActivityTeachingStrategy, ActivityBloomsTaxonomyLevel,
     ActivityDuration, ActivityJumpStartCoalition, ActivityCouncilForEconEd
 )
+
 import mock
 from model_mommy import mommy
 from v1.models import HomePage
-from django.test import RequestFactory, TestCase, override_settings
 from haystack.models import SearchResult
 from haystack.query import SearchQuerySet
-from django.http import Http404, HttpRequest, HttpResponse
 
 @override_settings(
     FLAGS={'TDP_SEARCH_INTERFACE': {'boolean': True}}
@@ -43,7 +50,7 @@ class ActivityIndexPageTests(WagtailPageTests):
         ROOT_PAGE = HomePage.objects.first()
         self.assertCanCreate(ROOT_PAGE, ActivityIndexPage, {
             'title': 'Search for activities',
-            'intro': "<p>Test Intro</p>",
+            'header-count': '0',
             'sidefoot-count': '0',
             'categories-TOTAL_FORMS': '0',
             'categories-INITIAL_FORMS': '0',
@@ -61,23 +68,20 @@ class TestActivityIndexPageSearch(TestCase):
         self.ROOT_PAGE.save_revision().publish()
         self.site = Site.objects.get(is_default_site=True)
         self.factory = RequestFactory()
-        self.search_page = ActivityIndexPage(live=True, path='search', depth='1', title='Search for activities', slug='search', intro='<p>Test Intro</p>')
+        self.search_page = ActivityIndexPage(
+            live=True,
+            path='search',
+            depth='1',
+            title='Search for activities',
+            slug='search'
+        )
+        self.search_page.header = StreamValue(self.search_page.header.stream_block, [atomic.text_introduction], True)
+        publish_page(child=self.search_page)
 
     def get_request(self, path='', data={}):
         request = self.factory.get(path, data=data)
         request.site = self.site
         return request
-
-    def test_client_can_create_default_home_page(self):
-        # Arrange
-        home_page = HomePage(title='Home page', slug='home-test-unique-slug')
-        self.ROOT_PAGE.add_child(instance=home_page)
-        home_page.save_revision().publish()
-        # Act
-        response = self.client.get('/home-test-unique-slug/')
-        # Assert
-        self.assertEqual(response.status_code, 200)
-
 
     def test_activity_index_page_renders(self):
         # Arrange
@@ -123,12 +127,15 @@ class TestActivityIndexPageSearch(TestCase):
         # Arrange
         my_request = self.search_page.dummy_request()
         my_request.environ["QUERY_STRING"]="q=voldemort"
-        activity_page = self.create_activity_detail_page(title='Planning for future savings', slug='planning-future-savings')
+        activity_page = self.create_activity_detail_page(
+            title='Planning for future savings',
+            slug='planning-future-savings'
+        )
         # Act
         response = self.search_page.serve(my_request)
         response.render()
         # Assert
-        self.assertTrue(b'<h3>No results match your search.</h3>' in response.content)
+        self.assertTrue(b'<h3>No results match your search.</h3>' in response.content)  # noqa: E501
 
     @mock.patch('teachers_digital_platform.models.pages.SearchQuerySet.models')
     def test_search_get_all_facets_with_building_block_filter(self, mock_sqs):
@@ -136,20 +143,18 @@ class TestActivityIndexPageSearch(TestCase):
         facet_counts = {
             'dates': {},
             'fields': {
-                    'topic':'1',
-                    'topic':'4',
-                    'building_block': '1',
-                    'building_block': '2',
-                    'school_subject': '1',
-                },
-                'queries': {}
-            }
+                'topic': ['1', '4'],
+                'building_block': ['1', '2'],
+                'school_subject': ['1'],
+            },
+            'queries': {}
+        }
         selected_facets = {u'building_block': [1]}
-        facet_queries = {'building_block':'building_block_exact'}
+        facet_queries = {'building_block': 'building_block_exact'}
         facet_map = self.create_facet_map()
         mock_sqs.facet_counts.return_value = facet_counts
         # Act
-        actual_all_facets = self.search_page.get_all_facets(facet_map, mock_sqs, facet_counts, facet_queries, selected_facets)
+        actual_all_facets = self.search_page.get_all_facets(facet_map, mock_sqs, facet_counts, facet_queries, selected_facets)  # noqa: E501
         # Assert
         self.assertTrue('building_block' in actual_all_facets)
 
@@ -197,47 +202,48 @@ class TestActivityIndexPageSearch(TestCase):
             ('building_block', (ActivityBuildingBlock, False, 10)),
             ('school_subject', (ActivitySchoolSubject, False, 25)),
             ('topic', (ActivityTopic, True, 25)),
-            ('grade_level', (ActivityGradeLevel, False, 10)),
+            ('grade_level', (ActivityGradeLevel, False, 10)),                           # noqa: E501
             ('age_range', (ActivityAgeRange, False, 10)),
-            ('student_characteristics', (ActivityStudentCharacteristics, False, 10)),
+            ('student_characteristics', (ActivityStudentCharacteristics, False, 10)),   # noqa: E501
             ('activity_type', (ActivityType, False, 10)),
-            ('teaching_strategy', (ActivityTeachingStrategy, False, 25)),
-            ('blooms_taxonomy_level', (ActivityBloomsTaxonomyLevel, False, 25)),
-            ('activity_duration', (ActivityDuration, False, 10)),
-            ('jump_start_coalition', (ActivityJumpStartCoalition, False, 25)),
-            ('council_for_economic_education', (ActivityCouncilForEconEd, False, 25)),
+            ('teaching_strategy', (ActivityTeachingStrategy, False, 25)),               # noqa: E501
+            ('blooms_taxonomy_level', (ActivityBloomsTaxonomyLevel, False, 25)),        # noqa: E501
+            ('activity_duration', (ActivityDuration, False, 10)),                       # noqa: E501
+            ('jump_start_coalition', (ActivityJumpStartCoalition, False, 25)),          # noqa: E501
+            ('council_for_economic_education', (ActivityCouncilForEconEd, False, 25)),  # noqa: E501
         )
 
     def create_activity_detail_page(self, title='title', slug='slug'):
         activity_page = ActivityPage(
-            live=True, 
+            live=True,
             title=title,
             slug=slug,
             path=slug,
             activity_file=mommy.make(Document),
             date="2018-07-31",
-            summary="Students will discuss short-term and long-term goals and what\r\nmakes a goal SMART. They\u2019ll then create a short-term savings goal\r\nand make a plan to meet that goal.",
-            big_idea="<p>Saving money is essential to a positive\u00a0financial future.</p>",
-            objectives="<ul><li>Understand the importance of setting SMARTsavings goals<br/></li><li>Create a short-term SMART savings goal</li><li>Make an action plan to save money</li></ul>",
-            essential_questions="<p></p><ul><li>How can I reach my savings goals?<br/></li></ul><p></p>",
-            what_students_will_do="<ul><li>Use the \u201cCreating a savings plan\u201d worksheet to\u00a0brainstorm a financial goal<br/></li><li>Create a SMART goal and a savings plan to\u00a0achieve this goal</li></ul>",
-            building_block=ActivityBuildingBlock.objects.filter(pk__in=[2]).all(),
-            school_subject=ActivitySchoolSubject.objects.filter(pk__in=[1, 4]).all(),
+            summary="Students will discuss short-term and long-term goals and what\r\nmakes a goal SMART. They\u2019ll then create a short-term savings goal\r\nand make a plan to meet that goal.",  # noqa: E501
+            big_idea="<p>Saving money is essential to a positive\u00a0financial future.</p>",           # noqa: E501
+            objectives="<ul><li>Understand the importance of setting SMARTsavings goals<br/></li><li>Create a short-term SMART savings goal</li><li>Make an action plan to save money</li></ul>",  # noqa: E501
+            essential_questions="<p></p><ul><li>How can I reach my savings goals?<br/></li></ul><p></p>",  # noqa: E501
+            what_students_will_do="<ul><li>Use the \u201cCreating a savings plan\u201d worksheet to\u00a0brainstorm a financial goal<br/></li><li>Create a SMART goal and a savings plan to\u00a0achieve this goal</li></ul>",  # noqa: E501
+            building_block=ActivityBuildingBlock.objects.filter(pk__in=[2]).all(),                      # noqa: E501
+            school_subject=ActivitySchoolSubject.objects.filter(pk__in=[1, 4]).all(),                   # noqa: E501
             topic=ActivityTopic.objects.filter(pk__in=[6, 11]).all(),
             grade_level=ActivityGradeLevel.objects.filter(pk__in=[2]).all(),
             age_range=ActivityAgeRange.objects.filter(pk__in=[2]).all(),
             student_characteristics=[],
             activity_type=ActivityType.objects.filter(pk__in=[1, 2, 3]).all(),
-            teaching_strategy=ActivityTeachingStrategy.objects.filter(pk__in=[6, 7]).all(),
-            blooms_taxonomy_level=ActivityBloomsTaxonomyLevel.objects.filter(pk__in=[6]).all(),
+            teaching_strategy=ActivityTeachingStrategy.objects.filter(pk__in=[6, 7]).all(),             # noqa: E501
+            blooms_taxonomy_level=ActivityBloomsTaxonomyLevel.objects.filter(pk__in=[6]).all(),         # noqa: E501
             activity_duration=ActivityDuration.objects.get(pk=2),
-            council_for_economic_education=ActivityCouncilForEconEd.objects.filter(pk__in=[4]).all(),
-            jump_start_coalition=ActivityJumpStartCoalition.objects.filter(pk__in=[1]).all()
+            council_for_economic_education=ActivityCouncilForEconEd.objects.filter(pk__in=[4]).all(),   # noqa: E501
+            jump_start_coalition=ActivityJumpStartCoalition.objects.filter(pk__in=[1]).all()            # noqa: E501
         )
         return activity_page
 
+
 # print_to_file(response.content, 'response.html')
 def print_to_file(text, filename):
-        handle1=open(filename,'w+')
+        handle1 = open(filename, 'w+')
         handle1.write(text)
         handle1.close()
--- teachers_digital_platform/wagtail_hooks.py
+++ teachers_digital_platform/wagtail_hooks.py
@@ -1,19 +1,12 @@
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.utils.html import format_html, format_html_join
 
 from mptt.admin import DraggableMPTTAdmin
 
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register
 )
-from wagtail.contrib.modeladmin.views import EditView
-from wagtail.wagtailcore import hooks
-from wagtail.wagtailcore.whitelist import attribute_rule
 
 from teachers_digital_platform.models import (
     ActivityBuildingBlock, ActivitySchoolSubject, ActivityTopic, ActivityGradeLevel, ActivityAgeRange,
@@ -95,6 +88,7 @@ class ActivityCouncilForEconEdModelAdmin(ModelAdmin):
     model = ActivityCouncilForEconEd
     menu_icon = 'list-ul'
     menu_label = 'Council for Economic Education'
+
 
 @modeladmin_register
 class MyModelAdminGroup(ModelAdminGroup):
