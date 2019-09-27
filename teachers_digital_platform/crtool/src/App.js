5bcb9d651fc6f3f32254793054ef18de59118ede
Norman Bucknor <nbucknor@forumone.com>
2019-09-27 15:12:27 -0400

    [CRTOOL] replace babel-polyfill

--- teachers_digital_platform/crtool/src/App.js
+++ teachers_digital_platform/crtool/src/App.js
@@ -1,11 +1,12 @@
-import "babel-polyfill";
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11'; // For IE 11 support
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import React, { Component } from 'react';
 
 import CustomerReviewToolComponent from "./js/components/CustomerReviewToolComponent";
 
-require("babel-polyfill");
+require('react-app-polyfill/ie11');
 
 class App extends Component {
   render() {
--- teachers_digital_platform/crtool/src/index.js
+++ teachers_digital_platform/crtool/src/index.js
@@ -1,4 +1,5 @@
-import "babel-polyfill";
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11'; // For IE 11 support
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import React from 'react';
