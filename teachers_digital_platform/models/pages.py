b68a4ad8c289ad73513c67d63b32cc3844392a42
Norman Bucknor <nbucknor@forumone.com>
2019-11-22 16:31:35 -0500

    [TDP] Add Similar Activities block to Activity detail page

--- teachers_digital_platform/models/pages.py
+++ teachers_digital_platform/models/pages.py
@@ -401,6 +401,21 @@ class ActivityPage(CFGOVPage):
         index.FilterField('council_for_economic_education'),
     ]
 
+    def get_subtopic_ids(self):
+        """
+        Get a list of this activity's subtopic ids
+        """
+        topic_ids = [topic.id for topic in self.topic.all()]
+        root_ids = ActivityTopic.objects.filter(id__in=topic_ids).filter(parent=None).values_list('id', flat=True)
+        return set(topic_ids) - set(root_ids)
+
+    def get_grade_level_ids(self):
+        """
+        Get a list of this activity's grade_level ids
+        """
+        grade_level_ids = [grade_level.id for grade_level in self.grade_level.all()]
+        return grade_level_ids
+
     def get_topics_list(self, parent=None):
         """
         Get a hierarchical list of this activity's topics.
