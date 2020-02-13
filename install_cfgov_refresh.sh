568543cdb9909600787a0cd04035848ece68286c
Craig Davies <craig.davies@cfpb.gov>
2020-02-13 11:43:01 -0500

    Upgrade pip

--- install_cfgov_refresh.sh
+++ install_cfgov_refresh.sh
@@ -2,6 +2,8 @@
 
 git clone --depth 1 https://github.com/cfpb/cfgov-refresh
 cd cfgov-refresh
+python -m pip install --upgrade pip
+pip install -r requirements/wagtail.txt
 
 # If cfgov-refresh includes this project in its list of requirements, we should
 # skip it when installing dependencies.
