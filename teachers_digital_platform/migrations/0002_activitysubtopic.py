bda6efb4d10056a84fe35d90b471eb4cdd0deca4
Craig Davies <craig.davies@cfpb.gov>
2020-03-30 21:52:23 -0400

    Add Django 2 import

--- teachers_digital_platform/migrations/0002_activitysubtopic.py
+++ teachers_digital_platform/migrations/0002_activitysubtopic.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -14,7 +15,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=255)),
-                ('parent', models.ForeignKey(related_name='subtopics', default=None, blank=True, to='teachers_digital_platform.ActivityTopic', null=True)),
+                ('parent', models.ForeignKey(related_name='subtopics', on_delete=django.db.models.deletion.SET_NULL, default=None, blank=True, to='teachers_digital_platform.ActivityTopic', null=True)),
             ],
             options={
                 'ordering': ['title'],
--- teachers_digital_platform/urls.py
+++ teachers_digital_platform/urls.py
@@ -1,27 +1,34 @@
-from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from wagtailsharing.views import ServeView
 
 
+try:
+    from django.urls import re_path
+except ImportError:
+    from django.conf.urls import url as re_path
+
+
 urlpatterns = [
-    url(
+    re_path(
         r"^curriculum-review/tool/$",
         TemplateView.as_view(
             template_name="teachers_digital_platform/crt-survey.html"
         ),
     ),
-    url(
+    re_path(
         r"^curriculum-review/before-you-begin/$",
         TemplateView.as_view(
             template_name="teachers_digital_platform/crt-start.html"
         ),
     ),
-    url(
+    re_path(
         r"^journey",
         TemplateView.as_view(
             template_name="teachers_digital_platform/bb-tool.html"
         ),
     ),
-    url(r"^$", lambda request: ServeView.as_view()(request, request.path),),
+    re_path(
+        r"^$", lambda request: ServeView.as_view()(request, request.path),
+    ),
 ]
