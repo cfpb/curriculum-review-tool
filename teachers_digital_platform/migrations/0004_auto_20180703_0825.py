8ca64d92394d24904be6af3c5c729c8409ae1cc7
Craig Davies <craig.davies@cfpb.gov>
2020-04-13 12:23:19 -0400

    Support import of Django 1.11 and 2.0 libraries
    - Support for Django 2.0 import paths
    - Add `on_delete` to the following lines in Django migrations:
    1. `models.ForeignKey(`
    2. `fields.TreeForeignKey(`
    3. `models.OneToOneField(`
    
    - `from __future__ import absolute_import, unicode_literals`

--- teachers_digital_platform/migrations/0004_auto_20180703_0825.py
+++ teachers_digital_platform/migrations/0004_auto_20180703_0825.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import migrations, models
+import django.db.models.deletion
 import mptt.fields
 
 
@@ -35,7 +34,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='activitytopic',
             name='parent',
-            field=mptt.fields.TreeForeignKey(related_name='children', blank=True, to='teachers_digital_platform.ActivityTopic', null=True),
+            field=mptt.fields.TreeForeignKey(related_name='children', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='teachers_digital_platform.ActivityTopic', null=True),
         ),
         migrations.AddField(
             model_name='activitytopic',
