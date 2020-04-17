c53878ae6487e718f168bc112ee0ac0ce41d2a9e
Craig Davies <craig.davies@cfpb.gov>
2020-04-17 15:19:22 -0400

    Fix setup so tox can run locally

--- setup.sh
+++ setup.sh
@@ -23,16 +23,18 @@ init() {
     CR_TOOL_DEP_CHECKSUM=$(cat teachers_digital_platform/crtool/package.json | shasum -a 256)
   fi
 
-  # if [[ "$(node -v)" != 'v10.'* ]] && [[ "$(node -v)" != 'v11.'* ]] && [[ "$(node -v)" != 'v12.'* ]]; then
-  if [[ "$(node -v)" != 'v10.'* -a "$(node -v)" != 'v11.'* -a "$(node -v)" != 'v12.'* -a "$(node -v)" != 'v13.'* ]]; then
+  if [[ "$(node -v)" != 'v10.'* ]] &&
+     [[ "$(node -v)" != 'v11.'* ]] &&
+     [[ "$(node -v)" != 'v12.'* ]] &&
+     [[ "$(node -v)" != 'v13.'* ]]; then
     printf "\033[1;31mPlease install Node 10.x or higher: 'nvm install 10'\033[0m\n"
   fi
 
   NODE_DIR=node_modules
-  echo "npm components directory: $NODE_DIR"
+  echo "npm components directory: ${NODE_DIR}"
 
   CRTOOL_NODE_DIR=teachers_digital_platform/crtool/node_modules
-  echo "crtool npm components directory: $CRTOOL_NODE_DIR"
+  echo "crtool npm components directory: ${CRTOOL_NODE_DIR}"
 }
 
 # Clean project dependencies.
@@ -40,15 +42,15 @@ clean() {
   # If the node directory already exists,
   # clear it so we know we're working with a clean
   # slate of the dependencies listed in package.json.
-  if [ -d $NODE_DIR ]; then
-    echo "Removing project dependency directory $NODE_DIR"
-    rm -rf $NODE_DIR
+  if [ -d ${NODE_DIR} ]; then
+    echo "Removing project dependency directory ${NODE_DIR}"
+    rm -rf ${NODE_DIR}
     echo "Project dependencies have been removed."
   fi
 
-  if [ -d $CRTOOL_NODE_DIR ]; then
-    echo "Removing project dependency directory $CRTOOL_NODE_DIR"
-    rm -rf $CRTOOL_NODE_DIR
+  if [ -d ${CRTOOL_NODE_DIR} ]; then
+    echo "Removing project dependency directory ${CRTOOL_NODE_DIR}"
+    rm -rf ${CRTOOL_NODE_DIR}
     echo "Project dependencies have been removed."
   fi
 }
@@ -61,8 +63,8 @@ install() {
 
 # Add a checksum file
 checksum() {
-  echo -n "$DEP_CHECKSUM" > $NODE_DIR/CHECKSUM
-  echo -n "$CR_TOOL_DEP_CHECKSUM" > $CRTOOL_NODE_DIR/CHECKSUM
+  echo -n "${DEP_CHECKSUM}" > ${NODE_DIR}/CHECKSUM
+  echo -n "${CR_TOOL_DEP_CHECKSUM}" > ${CRTOOL_NODE_DIR}/CHECKSUM
 }
 
 # If the node directory exists, $NODE_DIR/CHECKSUM exists, and
@@ -89,16 +91,16 @@ build() {
 }
 
 # Execute requested (or all) functions.
-if [ "$1" = "init" ]; then
+if [ "$1" == "init" ]; then
   init ""
   clean_and_install
-elif [ "$1" = "clean" ]; then
+elif [ "$1" == "clean" ]; then
   echo "Clean project dependencies."
   init ""
   clean
   clean_and_install
   build
-elif [ "$1" = "build" ]; then
+elif [ "$1" == "build" ]; then
   build
 else
   init "$1"
