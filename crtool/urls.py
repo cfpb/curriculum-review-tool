b2ca1cbae0a51cf6d3a0e3dc59b36e616e3f9cc8
Craig Davies <craig.davies@cfpb.gov>
2020-05-26 14:52:21 -0400

    Curriculum Review tool Django application

---
+++ crtool/urls.py
@@ -0,0 +1,20 @@
+from django.urls import re_path
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    re_path(
+        r'^curriculum-review/tool/$',
+        TemplateView.as_view(template_name='crtool/crt-survey.html')
+    ),
+
+    re_path(
+        r'^curriculum-review/before-you-begin/$',
+        TemplateView.as_view(template_name='crtool/crt-start.html')
+    ),
+
+    re_path(
+        r'^journey',
+        TemplateView.as_view(template_name='crtool/bb-tool.html')
+    )
+]
