e80e5fca8e3253a189253c94ecfe56598bf99ce0
Norman Bucknor <nbucknor@forumone.com>
2020-08-05 14:20:41 -0400

    [CRTool] fixing frontend linting issues

--- crtool/js/analytics.js
+++ crtool/js/analytics.js
@@ -1,46 +1,46 @@
 const category = 'curriculum review tool interaction';
 
-( function ( $ ) {
+( function( $ ) {
   // Tracks analytics for links on page clicked
-  $( 'button.link-push-analytics' ).click( function () {
-    const linkText  = $( this ).text().trim();
-    const action = 'link clicked: ' + linkText ;
-    buttonLinkClicked( action, linkText  );
-  });
+  $( 'button.link-push-analytics' ).click( function() {
+    const linkText = $( this ).text().trim();
+    const action = 'link clicked: ' + linkText;
+    buttonLinkClicked( action, linkText );
+  } );
 
-  $( 'button.button-push-analytics' ).click( function () {
-    const linkText  = $( this ).text().trim();
+  $( 'button.button-push-analytics' ).click( function() {
+    const linkText = $( this ).text().trim();
     const action = 'button clicked';
-    buttonLinkClicked( action, linkText  );
-  });
+    buttonLinkClicked( action, linkText );
+  } );
 
-  $( 'button.save-work-push-analytics' ).click( function () {
+  $( 'button.save-work-push-analytics' ).click( function() {
     const action = 'save work modal';
     const label = $( this ).text().trim();
     modelButtonClicked( action, label );
-  });
+  } );
 
-  $( 'button.start-over-push-analytics' ).click( function () {
+  $( 'button.start-over-push-analytics' ).click( function() {
     const action = 'Starting over modal';
     const label = $( this ).text().trim();
     modelButtonClicked( action, label );
   } );
 
-  $( 'button.start-over-close-push-analytics' ).click( function () {
+  $( 'button.start-over-close-push-analytics' ).click( function() {
     const action = 'Starting over modal';
     const label = $( this ).text().trim();
     modelButtonClicked( action, label );
   } );
 
-  $( 'a.push-download-analytics' ).click(function () {
-    const linkText  = $( this ).text().trim();
+  $( 'a.push-download-analytics' ).click( function() {
+    const linkText = $( this ).text().trim();
     const linkUrl = $( this ).attr('href');
-    sendAnalytics( linkText , linkUrl, 'Downloads' );
+    sendAnalytics( linkText, linkUrl, 'Downloads' );
   } );
 } )( jQuery );
 
-function buttonLinkClicked( action, linkText  ) {
-  sendAnalytics( action, linkText , category );
+function buttonLinkClicked( action, linkText ) {
+  sendAnalytics( action, linkText, category );
 }
 
 function modelButtonClicked( action, label ) {
