dde92c5200ef0ed7a6946f89b28792e8afdc76c8
Norman Bucknor <nbucknor@forumone.com>
2020-08-15 00:26:07 -0400

    [CRTool] Adding CurriculumReviewSession model and views

--- crtool/urls.py
+++ crtool/urls.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
 from django.views.generic import TemplateView
 
+from . import views
+
 
 urlpatterns = [
     re_path(
@@ -11,4 +13,20 @@ urlpatterns = [
         r'^tool/$',
         TemplateView.as_view(template_name='crtool/crt-survey.html')
     ),
+    re_path(
+        r'^create-review/$',
+        views.create_review,
+        name='create_review'
+    ),
+    re_path(
+        r'^get-review/$',
+        views.get_review,
+        name='get_review'
+    ),
+    re_path(
+        r'^update-review/$',
+        views.update_review,
+        name='update_review'
+    ),
+
 ]
--- crtool/views.py
+++ crtool/views.py
@@ -1,3 +1,82 @@
-# from django.shortcuts import render
+import json
+import uuid
+from datetime import datetime
+from pprint import pprint
 
-# Create your views here.
+from django.core.exceptions import ValidationError
+from django.http import Http404, JsonResponse, HttpResponse
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+
+from crtool.models import CurriculumReviewSession
+
+
+@csrf_exempt
+def create_review(request):
+    if request.method == 'POST':
+        fd = json.loads(request.body.decode("utf-8"))
+
+        title = fd['tdp-crt_title'] if 'tdp-crt_title' in fd else ''
+        pub_date = fd['tdp-crt_pubdate'] if 'tdp-crt_pubdate' in fd else ''
+        grade_range = fd['tdp-crt_grade'] if 'tdp-crt_grade' in fd else ''
+        passcode = fd['tdp-crt_pass_code'] if 'tdp-crt_pass_code' in fd else ''
+        review_id = uuid.uuid4()
+        last_updated = timezone.now()
+
+        if not title or not grade_range:
+            return HttpResponse(status=400)
+        data = {
+            'id': str(review_id),
+            'pass_code': passcode,
+            'last_updated': str(datetime.isoformat(last_updated)),
+            'curriculumTitle': title,
+            'publicationDate': pub_date,
+            'gradeRange': grade_range
+        }
+
+        review = CurriculumReviewSession.objects.create(
+            id=review_id,
+            pass_code=passcode,
+            last_updated=last_updated,
+            data=data
+        )
+
+        if review:
+            return JsonResponse(review.data)
+
+    else:
+        return HttpResponse(status=400)
+
+
+def get_review(request):
+    data = {}
+    if request.method == 'GET':
+        review_id = request.GET.get('tdp-crt_id')
+        try:
+            review = CurriculumReviewSession.objects.get(id=review_id)
+            if review:
+                data = review.data
+        except (CurriculumReviewSession.DoesNotExist, ValueError, ValidationError):
+            return HttpResponse(status=404)
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def update_review(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode("utf-8"))
+        if "id" in data:
+            try:
+                review = CurriculumReviewSession.objects.get(id=data["id"])
+                if review:
+                    # Update last_updated date
+                    last_updated = timezone.now()
+                    data['last_updated'] = str(datetime.isoformat(last_updated))
+                    review.data = data
+                    review.last_updated = last_updated
+                    review.save()
+                    return JsonResponse(review.data)
+            except (CurriculumReviewSession.DoesNotExist, ValueError, ValidationError):
+                return HttpResponse(status=404)
+
+    return HttpResponse(status=404)
