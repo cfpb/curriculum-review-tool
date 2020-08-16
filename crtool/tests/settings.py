3284ccf1b07f9fa43b5323dd07a1126bbcba6f27
Norman Bucknor <nbucknor@forumone.com>
2020-08-16 14:59:19 -0400

    Set ROOT_URLCONF

--- crtool/tests/settings.py
+++ crtool/tests/settings.py
@@ -1,2 +1,11 @@
-from cfgov.settings.test import *  # noqa: F403 F401
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crtool'
+]
+SECRET_KEY = "not-secret-key-for-testing"
 
+ROOT_URLCONF = "crtool.urls"
