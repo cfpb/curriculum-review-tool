ba1d302538fc66bf7a8773bea247858e1c91d35b
Norman Bucknor <nbucknor@forumone.com>
2020-08-16 14:51:20 -0400

    Test with original test settings file

--- crtool/tests/settings.py
+++ crtool/tests/settings.py
@@ -1,9 +1,2 @@
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'crtool'
-]
-SECRET_KEY = "not-secret-key-for-testing"
+from cfgov.settings.test import *  # noqa: F403 F401
+
