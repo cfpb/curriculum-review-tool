d6c1928c5df52f814327e2601c4c8efaa12980c5
Norman Bucknor <nbucknor@forumone.com>
2020-08-28 01:49:26 -0400

    Switch CurriculumReviewSession id to short token field

--- crtool/models.py
+++ crtool/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import uuid
+import random
+import string
 
 from django.contrib.postgres.fields import JSONField
 from django.db import models
@@ -8,7 +9,19 @@ from django.utils import timezone
 
 class CurriculumReviewSession(models.Model):
     """Session state of a Curriculum Review"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=7, editable=False)
     pass_code = models.CharField(max_length=50, null=True, blank=True)
     data = JSONField()
     last_updated = models.DateTimeField('Updated', default=timezone.now)
+
+    def id_generator(size=7, chars=string.ascii_lowercase + string.digits):
+        temp_id = ''.join(random.choice(chars) for _ in range(size))
+        while CurriculumReviewSession.objects.filter(id=temp_id).exists():
+            temp_id = id_generator(size, chars)
+        return temp_id
+
+    def save(self, **kwargs):
+        if not self.id:
+            # Generate ID once, then check the db. If exists, keep trying.
+            self.id = self.id_generator()
+        super(CurriculumReviewSession, self).save(**kwargs)
