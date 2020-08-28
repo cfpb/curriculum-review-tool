cc10f9004ddbb54c03fb200b42c5ac6f6587ed0d
Norman Bucknor <nbucknor@forumone.com>
2020-08-28 00:04:20 -0400

    Remove sqlite3 jsonfield and use postgres for testing

--- crtool/models.py
+++ crtool/models.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import uuid
 
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils import timezone
-
-from crtool.fields import JSONField
 
 
 class CurriculumReviewSession(models.Model):
