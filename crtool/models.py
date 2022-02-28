54cb934d0002b0fcd5996c9641d3ebd545db1adf
Justin Slay <justin.slay@cfpb.gov>
2022-02-28 14:30:01 -0700

    Fix linting and migration model import

--- crtool/models.py
+++ crtool/models.py
@@ -6,6 +6,7 @@ from django import VERSION as DJANGO_VERSION
 from django.db import models
 from django.utils import timezone
 
+
 if DJANGO_VERSION[0] < 3:
     from django.contrib.postgres.fields import JSONField
 else:
